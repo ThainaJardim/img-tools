42ee72298a9762b1a50a40b0f69681e2228153d2
ThainaJardim <thainaj.aguiar@gmail.com>
2024-07-11 09:33:59 -0300

    chore: Refactor revert commit handling and improve detection in detect_reverts.py

--- scripts/detect_reverts.py
+++ scripts/detect_reverts.py
@@ -11,14 +11,28 @@ def get_commit_diffs(oldrev, newrev):
     commits = result.stdout.strip().split('\n')
     return commits
 
-def get_all_commits_since(since_date):
-    result = subprocess.run(
-        ["git", "rev-list", "--all","--merges", f"--since={since_date}", "--reverse"],
+def get_main_or_master_branch():
+    branches = subprocess.run(
+        ["git", "branch", "--list", "main", "master"],
         capture_output=True,
         text=True
     )
-    all_commits = result.stdout.strip().split('\n')
-    return all_commits
+    branches = branches.stdout.strip().split('\n')
+    if "main" in branches:
+        return "main"
+    elif "master" in branches:
+        return "master"
+    else:
+        raise ValueError("Neither 'main' nor 'master' branch found in the repository")
+
+def get_merges_in_branch_since(branch, since_date):
+    result = subprocess.run(
+        ["git", "rev-list", branch, "--merges", f"--since={since_date}", "--reverse"],
+        capture_output=True,
+        text=True
+    )
+    merges = result.stdout.strip().split('\n')
+    return merges
 
 def get_diff_between_commits(commit1, commit2):
     result = subprocess.run(
@@ -28,8 +42,8 @@ def get_diff_between_commits(commit1, commit2):
     )
     return result.stdout.strip()
 
-def is_revert(commit, all_commits):
-    for potential_original_commit in all_commits:
+def is_revert(commit, merges):
+    for potential_original_commit in merges:
         if commit == potential_original_commit:
             continue
         
@@ -51,15 +65,18 @@ def main():
     # Calcular a data de 6 meses atrás
     since_date = get_date_six_months_ago()
 
-    # Obter todos os commits desde a data calculada
-    all_commits = get_all_commits_since(since_date)
+    # Determinar a branch principal (main ou master)
+    branch = get_main_or_master_branch()
+
+    # Obter todos os commits de merge na branch principal desde a data calculada, em ordem cronológica reversa (mais recentes primeiro)
+    merges = get_merges_in_branch_since(branch, since_date)
 
     # Obter os commits entre as revisões especificadas
     commits = get_commit_diffs(oldrev, newrev)
 
     # Verificar se algum commit recente é um revert
     for commit in commits:
-        if is_revert(commit, all_commits):
+        if is_revert(commit, merges):
             print(commit)
             return commit  # Retorna o hash do commit que é um revert
 
